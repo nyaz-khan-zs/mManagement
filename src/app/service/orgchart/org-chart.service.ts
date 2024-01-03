45cc5bd14f1aa27951560636681719b7458f0172
iliyaz ali <shaikiliyaz.ali@zospmart.com>
2024-01-03 16:29:21 +0530

    org chart error fixed

--- src/app/service/orgchart/org-chart.service.ts
+++ src/app/service/orgchart/org-chart.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Node, User } from '../models/org.model';
-import { data } from '../';
+import { Node, User } from '../../core/models/org.model';
+import { data } from '../../mock/response';
 
 @Injectable({
   providedIn: 'root',
